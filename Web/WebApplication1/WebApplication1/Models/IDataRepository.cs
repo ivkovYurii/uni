bbc0e23cc14a3ec4f67541cda48f39af077b5ba1
vladyslav-keidaliuk <vladislavdocuments@gmail.com>
2023-09-14 01:22:31 +0300

    Lection 2 (13.09.23) Connected DB (MySQL by defult), added EF
    Supported 2 DB (MS SQL and MySQL)
    
    P.S. Actions Create,Edit,Delete are not realized. So you can see errors, but the project should run.

---
+++ Web/WebApplication1/WebApplication1/Models/IDataRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1.Models;
+
+public interface IDataRepository
+{
+    Product GetProduct(int id);
+    
+    IEnumerable<Product> GetAllProducts();
+    
+    void DeleteProduct(int id);
+    void UpdateProduct(Product product);
+    void CreateProduct(Product product);
+}
